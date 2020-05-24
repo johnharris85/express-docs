5defbad7ed6ee39395644b4bfb7fd170b2c0bac9
John Harris <john@johnharris.io>
2020-05-24 10:42:00 -0700

    Nina improvements

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-express-mongoose-docs
+express-docs
 =====================
 
-[![Build Status](https://travis-ci.org/nabeel-ahmad/express-mongoose-docs.svg?branch=master)](https://travis-ci.org/nabeel-ahmad/express-mongoose-docs)
-[![Coverage Status](https://coveralls.io/repos/nabeel-ahmad/express-mongoose-docs/badge.png?branch=master)](https://coveralls.io/r/nabeel-ahmad/express-mongoose-docs?branch=master)
+This is a heavily-modified fork of [express-mongoose-docs](https://github.com/nabeel-ahmad/express-mongoose-docs) which removes the mongoose pieces and adds:
 
-This module will take away the pain of writing API and Data Model documentation for your Express & Mongoose based REST API.
+- configurable docs endpoint
+- recursive route handling (carried PR from [here](https://github.com/nabeel-ahmad/express-mongoose-docs/pull/8/files))
+
 It auto-generates API documentation from the code on runtime so the documentation always stays up to date.
 
 
@@ -14,7 +15,7 @@ Installation
 * Step 1 : Install
 
 ```sh
-npm install express-mongoose-docs
+npm install https://github.com/johnharris85/express-docs.git
 ```
 
 * Step 2 : Configure
@@ -22,27 +23,15 @@ npm install express-mongoose-docs
 Add these lines to your app.js file
 
 ```sh
-var docs = require("express-mongoose-docs");
+var docs = require("express-docs");
 ```
 
-Make sure the following line comes after all express middleware such as app.use(express.bodyParser());
+Make sure the following line comes after all express middleware such as app.use(express.json());
 
 ```sh
-docs(app, mongoose); // 2nd param is optional
+docs(app, "apiDocs"); // 2nd param is optional and is the path you want to serve the docs on.
 ```
 
-That's it. The Docs web page should be accessible at Your-Base-URL/apiDocs
+That's it. The Docs web page should be accessible at <base_url>/<docs_route>.
 
 Example: http://localhost:5000/apiDocs
-
-Screenshots
-===========
-
-![routes](https://raw.github.com/nabeel-ahmad/express-mongoose-docs/master/screenshots/screenshot1.png "Routes")
-
-![data-model](https://raw.github.com/nabeel-ahmad/express-mongoose-docs/master/screenshots/screenshot2.png "Data Model")
-
-
-
-[repository]:https://github.com/nabeel-ahmad/express-mongoose-docs
-
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var _ = require("underscore");
 var express = require("express");
 
-module.exports = function (app, mongoose) {
+module.exports = function (app, apiRoute) {
 
+    function appendRoutes(route, arr) {
+        if (!_.isUndefined(route.route)) {
+            route.route.method = Object.keys(route.route.methods).toString();
+            arr.push(route.route);
+        } else if(route.handle.stack) {
+            // Extract routes from middlewere installed Router
+            console.log(route.handle.stack)
+            _.each(route.handle.stack, function (route) {
+                appendRoutes(route, arr);
+            });
+        }
+    }
 
     // Add an API endpoint to be used internally by this module
-    app.get('/api-docs', function (req, res) {
+    app.get(`/${apiRoute}`, function (req, res) {
         try {
             if (app.routes) {
                 // Extract all API routes in one array  in case of express3
@@ -15,18 +27,7 @@ module.exports = function (app, mongoose) {
                 // Extract all API routes in one array  in case of express4
                 var arr = [];
                 _.each(app._router.stack, function (route) {
-                    if (!_.isUndefined(route.route)) {
-                        route.route.method = Object.keys(route.route.methods).toString();
-                        arr.push(route.route);
-                    } else if(route.handle.stack) {
-                        // Extract routes from middlewere installed Router
-                        _.each(route.handle.stack, function (route) {
-                            if (!_.isUndefined(route.route)) {
-                                route.route.method = Object.keys(route.route.methods).toString();
-                                arr.push(route.route);
-                            }
-                        });
-                    }
+                    appendRoutes(route, arr);
                 });
                 routes = arr;
             }
@@ -36,17 +37,12 @@ module.exports = function (app, mongoose) {
             });
 
             // Skip the routes to be used internally by this module
-            delete routes['api-docs'];
+            delete routes[apiRoute];
 
             // Transform route groups object to an array of group/routes pairs
             routes = _.pairs(routes);
 
-            var schemas;
-
-            if (mongoose)
-                schemas = generateSchemaDocs(mongoose);
-
-            res.send({routes: routes, schemas: schemas});
+            res.send({routes});
         } catch (e) {
             res.send(400,e);
         }
@@ -55,79 +51,3 @@ module.exports = function (app, mongoose) {
     // Configure the directory which holds html docs template
     app.use(express.static(__dirname + '/html'));
 };
-
-var nestedSchemas;
-
-// Transform mongoose model schemas into a readable format
-function generateSchemaDocs(mongoose) {
-
-    nestedSchemas = [];
-
-    // Transform models object to an array
-    var schemas = _.pairs(mongoose.modelSchemas);
-
-    // Map each schema to a readable format
-    schemas = _.map(schemas, function (schema) {
-        var info = getSchemaInfo(schema);
-        return info;
-    });
-
-    // Add nested schemas
-    schemas = schemas.concat(nestedSchemas);
-    return schemas;
-}
-
-function getSchemaInfo(schema) {
-
-    // Extract schema info for all fields of a schema
-    var paths = _.map(schema[1].paths, function (path) {
-
-        // Extract field info like type, required, enum etc.
-        var info = getFieldInfo(path);
-
-        // If field is a nested array with a custom, add it's schema to nested schemas
-        if (info && info.schema)
-            nestedSchemas.push(info.schema);
-
-        return info;
-    });
-
-    // Add virtual fields to schema info
-    _.each(schema[1].virtuals, function (virtual) {
-        if (virtual.path != "id")
-            paths.push({name: virtual.path, type: "Unknown"});
-    });
-
-    return {name: schema[0], fields: paths};
-}
-
-
-function getFieldInfo(path) {
-
-    var field = {name: path.path, type: path.instance};
-
-    if (path.options.type) {
-        field.type = path.options.type.name;
-
-        if (path.options.type instanceof Array && !path.schema)
-            field.type = path.options.type[0].name + " []";
-    }
-
-    field.min = path.options.min;
-    field.max = path.options.max;
-
-    if (path.enumValues && path.enumValues.length > 0)
-        field.enumValues = path.enumValues;
-
-    if (path.schema) {
-        // This field is a nested array with a custom schema
-        field.type = field.name;
-        // Get schema info for the array item schema
-        field.schema = getSchemaInfo([field.name, path.schema]);
-    }
-
-    if (path.isRequired)
-        field.required = true;
-
-    return field;
-}
